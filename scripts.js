fe3bebfc3229d7b23cfef99a5a4b75e35b8a3552
Jessica <jvictoriaw@hotmail.co.uk>
2024-02-21 17:38:22 +0000

    added a fucntion that plays a single round of rock paper scissors

--- scripts.js
+++ scripts.js
@@ -5,10 +5,28 @@ function getComputerChoice() {
 
 // console.log(getComputerChoice());
 
-// function play(playerSelection, computerSelection) {
-//     playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
 
-//     if (playerSelection == 'rock' && computerSelection == 'scissors') {
-//         return "You win! Rock beats scissors"
-//     }
-// }
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "You win! Rock beats scissors";
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You lose! Paper beats rock";
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return "You win! Paper bears rock";
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You lose! Scissors beats paper"
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return "You lose! Rock beats scissors"
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You win! Scissors beats paper"
+    } else if (playerSelection == computerSelection) {
+        return "Draw!"
+    } else {
+        return "Error!"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
